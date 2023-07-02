fed709da7d8443f8b2fe387178acce777aa7e642
Junaed <junaed1995@gmail.com>
2023-07-02 20:35:41 +0600

    Initial Commit

---
+++ Swiftui Animation Example 2/Swiftui_Animation_Example_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftui_Animation_Example_2App.swift
+//  Swiftui Animation Example 2
+//
+//  Created by Junaed Muhammad Chowdhury on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Swiftui_Animation_Example_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
