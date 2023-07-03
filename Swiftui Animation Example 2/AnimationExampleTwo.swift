7f79a4a8ae990bbb6dc493ee5c1b300b879b66cb
Junaed <junaed1995@gmail.com>
2023-07-03 11:30:47 +0600

    * Animating binding complete

--- Swiftui Animation Example 2/AnimationExampleTwo.swift
+++ Swiftui Animation Example 2/AnimationExampleTwo.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct AnimationExampleTwo: View {
+    @State private var animationAmount = 1.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                .easeInOut(duration: 3)
+                .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            
+            
+            Spacer()
+            Text(String(format: "%.0f",animationAmount))
+                .font(.title)
+            Spacer()
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+
+        }
+        .padding()
     }
 }
 
