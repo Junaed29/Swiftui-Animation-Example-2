4b7e016ec18c3fb96adc5ecb6c221b59378c5855
Junaed <junaed1995@gmail.com>
2023-07-03 11:46:50 +0600

    * Explicit animation added

--- Swiftui Animation Example 2/AnimationExampleThree.swift
+++ Swiftui Animation Example 2/AnimationExampleThree.swift
@@ -8,8 +8,19 @@
 import SwiftUI
 
 struct AnimationExampleThree: View {
+    @State private var animationAmount = 0.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("Tap Me") {
+            withAnimation (.interpolatingSpring(stiffness: 5, damping: 1)) {
+                animationAmount += 360
+            }
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
     }
 }
 
